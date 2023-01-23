aed8e17b3314d0b5ca7dd45b243f60e5388ff139
sophia <sophia.munoz@>
2023-01-23 12:20:30 -0300

    Initial Commit

---
+++ SwiftCourseUnitTestUdemy/SwiftCourseUnitTestUdemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCourseUnitTestUdemyApp.swift
+//  SwiftCourseUnitTestUdemy
+//
+//  Created by Sophia Paulette Muñoz Pailamilla on 23-01-23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCourseUnitTestUdemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
